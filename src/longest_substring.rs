beb5e35d10fbf2a0be39f8a22f02c50453c98e82
Hoang Van Loc <lochoang75@gmail.com>
2025-12-20 11:52:58 +0700

    Start build os from rust

---
+++ src/longest_substring.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let input = "abcabcbb";
+
+    // Call your function here
+    let result = length_of_longest_substring(input);
+
+    // Print the result
+    println!("The length of the longest substring without repeating characters is: {}", result);
+}
+
+fn length_of_longest_substring(s: &str) -> usize {
+    // Your logic here
+    let mut hash_set: HashSet()
+    0
+}
